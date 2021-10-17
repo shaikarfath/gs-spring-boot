9bbcbec451556f658f27a0c312a596071b7a2c63
shaikarfath <shaik_arfath@columbusstate.edu>
2021-10-17 21:13:12 +0000

    nasaapi

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -27,17 +27,17 @@ public class HelloController {
     // Request parameter method
 	@GetMapping("/xkcdSpec")
 	public String xkcdSpecific(@RequestParam(name="id", required=false) String id, Model model) {
-		if(id == null){
-			id = "200";
-		}
+		// if(id == null){
+		// 	id = "200";
+		// }
 		model.addAttribute("id", id);
 		return "xkcdSpecific";
 	}
 
 	@GetMapping("/nasaapod")
-	public String nasaApod(@RequestParam(name="date", required=false) String date, Model model) {
-		// if(date == null){
-		// 	date = "65VM0mvvF4QCsovmTQ86mbsLHndn14oLLWw61lgi";
+	public String nasaApod(@RequestParam(name = "api_key", required=true) String api_key,@RequestParam(name="date", required=false) String date, Model model) {
+		// if(api_key == null){
+		// 	api_key = "65VM0mvvF4QCsovmTQ86mbsLHndn14oLLWw61lgi";
 		// }
 		model.addAttribute("date", date);
 		return "nasaApod";
@@ -48,6 +48,12 @@ public class HelloController {
 	// public String xkcdSpecific(@PathVariable String id, Model model) {
 	// 	model.addAttribute("id", id);
 	// 	return "xkcdSpecific";
+	// }
+
+	// @GetMapping("/nasaapod/{date}")
+	// public String nasaApod(@PathVariable String date, Model model) {
+	// 	model.addAttribute("date", date);
+	// 	return "nasaApod";
 	// }
 
 }
